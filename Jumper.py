06f9bfd0acab5c6d0c6d8d275fd2e93a8a087a1c
richie17777 <asa21006@byui.edu>
2022-02-09 20:25:47 +0000

    Edited the gusser-b.py file

--- Jumper.py
+++ Jumper.py
@@ -1,8 +1,10 @@
 import string
 
-class Director: 
-    '''Class that directs the game. Initiates game play
-    and makes it flow the way it should.'''
+
+class Director:
+    """Class that directs the game. Initiates game play
+    and makes it flow the way it should."""
+
     def __init__(self):
         self.is_playing = True
         self._wordarray = WordArray()
@@ -10,44 +12,46 @@ class Director:
         self._terminal_service = TerminalService()
 
     def start_game(self):
-        '''During game play the director does inputs, updates, and outputs.'''
+        """During game play the director does inputs, updates, and outputs."""
         while self.is_playing:
             self._get_inputs()
             self._do_updates()
             self._do_outputs()
 
     def _get_inputs(self):
-        '''Reads letter.'''
+        """Reads letter."""
         new_letter = self._terminal_service.read_letter("/nGuess a letter [a-z]: ")
 
-    
     def _do_updates(self):
-        '''Keeps track of guesses'''
-        
+        """Keeps track of guesses"""
+
     def _do_outputs(self):
-        hint = self._wordarray.
+        hint = self._wordarray
 
 
 class Guesser:
     def __init__():
         pass
 
+
 class GameBoard:
     def __init__():
         pass
+
     def GameBoard(board):
-        print('  ___  ')
-        print(' /___\\')
-        print(' \\   / ')
-        print('  \\ /  ')
-        print('   0   ')
-        print('  /|\\ ')
-        print('  / \\ ')
+        print("  ___  ")
+        print(" /___\\")
+        print(" \\   / ")
+        print("  \\ /  ")
+        print("   0   ")
+        print("  /|\\ ")
+        print("  / \\ ")
         print()
-        print('^^^^^^^')
+        print("^^^^^^^")
         print()
 
-class TerminalService():
+
+class TerminalService:
     def __init__():
         pass
 
@@ -57,7 +61,84 @@ class WordArray:
         pass
 
     def Array(self):
-        self.array = ["Hello", "Shire", "Bring", "Acorn", "Please", "Close", "Front", "Frame", "Zebra", "Libra", "Speed", "Creed", "Anime", "Silly", "Smart", "Crazy", "Learn", "Stern", "Koala", "Brand", "Stand",  "Happy", "Style", "Miles", "Arrow", "Drama", "Allow", "Apply", "Child", "Crime", "Dress", "Dream", "Drink", "Enemy", "Entry", "Focus", "Fruit", "Glass", "Green", "Group", "Heart", "Guide", "Jones", "Judge", "Knife", "Layer", "March", "Major", "Metal", "Money", "Month", "Motor", "Music", "Panel", "Owner", "North", "Night", "Plane", "Plant", "Point", "Power", "Reply", "River", "Round", "Route", "Rugby", "Scale", "Scene", "Shape", "Smoke", "Sound", "South", "Space", "Sport", "Stock"]
+        self.array = [
+            "Hello",
+            "Shire",
+            "Bring",
+            "Acorn",
+            "Please",
+            "Close",
+            "Front",
+            "Frame",
+            "Zebra",
+            "Libra",
+            "Speed",
+            "Creed",
+            "Anime",
+            "Silly",
+            "Smart",
+            "Crazy",
+            "Learn",
+            "Stern",
+            "Koala",
+            "Brand",
+            "Stand",
+            "Happy",
+            "Style",
+            "Miles",
+            "Arrow",
+            "Drama",
+            "Allow",
+            "Apply",
+            "Child",
+            "Crime",
+            "Dress",
+            "Dream",
+            "Drink",
+            "Enemy",
+            "Entry",
+            "Focus",
+            "Fruit",
+            "Glass",
+            "Green",
+            "Group",
+            "Heart",
+            "Guide",
+            "Jones",
+            "Judge",
+            "Knife",
+            "Layer",
+            "March",
+            "Major",
+            "Metal",
+            "Money",
+            "Month",
+            "Motor",
+            "Music",
+            "Panel",
+            "Owner",
+            "North",
+            "Night",
+            "Plane",
+            "Plant",
+            "Point",
+            "Power",
+            "Reply",
+            "River",
+            "Round",
+            "Route",
+            "Rugby",
+            "Scale",
+            "Scene",
+            "Shape",
+            "Smoke",
+            "Sound",
+            "South",
+            "Space",
+            "Sport",
+            "Stock",
+        ]
+
 
 def main():
     director = Director()
